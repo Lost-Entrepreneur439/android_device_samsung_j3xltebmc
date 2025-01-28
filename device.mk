5a4d82283b6da741b542fca71368b4837b50a578
Ivy <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2025-01-27 21:40:21 -0330

    Revert "j3xltebmc: Enable LiveDisplay"
    This reverts commit 76a1ac5b27a49975bcb6734d8323ae7e878630cd.

--- device.mk
+++ device.mk
@@ -50,10 +50,6 @@ PRODUCT_PACKAGES += \
     camera.universal3475 \
     Snap
 
-# hardware/samsung/AdvancedDisplay (MDNIE)
-PRODUCT_PACKAGES += \
-    AdvancedDisplay
-
 # Graphics
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := large normal
